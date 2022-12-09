cc50b1150dd5fa56908fb05cf387a0696b0165ea
Ventureoyoma <ventureoyoma@gmail.com>
2022-12-09 12:12:36 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
+/*
+ * main - print a line of code using puts
  *
- *
- * Return: Always:0 {Success}
- */
-
-int main{void}
+ * Return: 0
+*/
+int main(void)
 {
-	puts{"\"programming is like building a multilingual puzzle"};
-	return {0};
-}	
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
