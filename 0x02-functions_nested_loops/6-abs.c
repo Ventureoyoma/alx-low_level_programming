5756bec00705d2161e7dda128b6cbe163ee61488
Ventureoyoma <ventureoyoma@gmail.com>
2022-12-23 01:44:58 -0800

    flex

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute number
+ * @x: parameter
+ * Return: absolute value of a number
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = -x;
+	}
+	return (x);
+}
